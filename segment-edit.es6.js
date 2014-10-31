e97ca47a133a7a1828b42724ad10262a12bd52c6
b-ma <benjamin.matuszewski@gmail.com>
2014-10-31 10:23:33 +0100

    responsible for handlers

--- segment-edit.es6.js
+++ segment-edit.es6.js
@@ -1,16 +1,74 @@
 var SegmentVis   = require('segment-vis');
 var makeEditable = require('make-editable');
+var extend       = require('utils').extend;
 
 'use strict';
 
 class SegmentEdit extends SegmentVis {
 
   constructor() {
-    if (!(this instanceof SegmentEdit)) return new SegmentEdit;
+    if (!(this instanceof SegmentEdit)) { return new SegmentEdit; }
 
     super();
     // default editable properties
-    this.param('edits', ['x', 'y', 'width', 'height']);
+    this.params({
+      edits: ['x', 'y', 'width', 'height'],
+      handlerWidth: 3,
+      handlerOpacity: 0
+    });
+  }
+
+  // add handler on segment shape
+  update(data) {
+    var g = super.update(data);
+
+    g.append('line')
+      .attr('class', 'handle left')
+      .attr('stroke-width', this.param('handlerWidth'))
+      .attr('stroke-opacity', this.param('handlerOpacity'));
+
+    g.append('line')
+      .attr('class', 'handle right')
+      .attr('stroke-width', this.param('handlerWidth'))
+      .attr('stroke-opacity', this.param('handlerOpacity'));
+  }
+
+  draw(el) {
+    el = super.draw(el);
+    var accessors = this.getAccesors();
+
+    el.selectAll('.handle.left')
+      .attr('x1', accessors.lhx)
+      .attr('x2', accessors.lhx)
+      .attr('y1', accessors.y)
+      .attr('y2', accessors.hh)
+      .style('stroke', accessors.color);
+
+    el.selectAll('.handle.right')
+      .attr('x1', accessors.rhx)
+      .attr('x2', accessors.rhx)
+      .attr('y1', accessors.y)
+      .attr('y2', accessors.hh)
+      .style('stroke', accessors.color);
+  }
+
+  getAccesors() {
+    var accessors = super.getAccessors();
+
+    var _handlerWidth = parseInt(this.param('handlerWidth'), 10)
+    var _halfHandler = _handlerWidth * 0.5;
+
+    // handler positions
+    var hh  = (d) => { return accessors.y(d) + accessors.h(d); }
+    var lhx = (d) => { return accessors.x(d) + _halfHandler; }
+    var rhx = (d) => {
+      let width = accessors.w(d);
+
+      return (width < (_handlerWidth * 2)) ?
+        accessors.x(d) + _handlerWidth + this.__minWidth : accessors.x(d) + width - _halfHandler;
+    }
+
+    return extend(accessors, { hh: hh, lhx: lhx, rhx: rhx });
   }
 
   // logic performed to select an item from the brush
@@ -63,7 +121,8 @@ class SegmentEdit extends SegmentVis {
 
   // checks if the clicked item is one of our guys
   clicked(item) {
-    return item.classList.contains('seg') || item.tagName === 'line';
+    return item.classList.contains(this.param('rect-class')) ||
+           item.tagName === 'line';
   }
 
   // mouse drag event switcher depending on drag (left|right|block) levels
@@ -83,7 +142,9 @@ class SegmentEdit extends SegmentVis {
     var d = res.d;
     var delta = res.event;
     var item = res.target;
+
     var xScale = this.base.xScale;
+    var yScale = this.yScale;
 
     var constrains = this.param('edits');
     var canX = !!~constrains.indexOf('x');
@@ -103,34 +164,44 @@ class SegmentEdit extends SegmentVis {
       this.base.svg.classed('handle-drag', true);
     }
 
-    var duration = xScale(_duration(d));
-    var begin = xScale(_start(d));
+    var width = xScale(_duration(d));
+    var posX = xScale(_start(d));
 
     // handle resize
-    if (mode === 'l' && canW) duration -= delta.dx; // px
-    if (mode === 'r' && canW) duration += delta.dx; // px
+    if (mode === 'l' && canW) width -= delta.dx; // px
+    if (mode === 'r' && canW) width += delta.dx; // px
 
     // apply duration when editing through the handles
     if ((mode === 'l' || mode === 'r') && canW) {
-      if (duration < 1) { duration = 1; }
-      _duration(d, xScale.invert(duration));
+      if (width < 1) { width = 1; }
+      _duration(d, xScale.invert(width));
     }
 
     if (mode === 'l' && canW) {
-      begin += delta.dx;
-      _start(d, xScale.invert(begin));
+      posX += delta.dx;
+      _start(d, xScale.invert(posX));
     }
 
     // handle move
     if (mode === 'mv' && canX) {
-      begin += delta.dx;
-      _start(d, xScale.invert(begin));
+      var minX = 0;
+      var maxX = this.base.width();
+      var targetX = posX + delta.dx;
+
+      if (targetX >= minX && (targetX + width) <= maxX) {
+        posX = targetX;
+        _start(d, xScale.invert(posX));
+      }
     }
 
     if (mode === 'mv' && canY) {
-      var y = this.yScale(_y(d));
-      y += delta.dy;
-      _y(d, this.yScale.invert(y));
+      var posY = this.yScale(_y(d)); // - (this.param('height') - this.yScale(this.height()(d)));
+      var minY = 0;
+      var maxY = this.param('height');
+      // console.log(posY)
+
+      posY += delta.dy;
+      _y(d, this.yScale.invert(posY));
     }
 
     // redraw visualization
--- segment-edit.js
+++ segment-edit.js
@@ -1,17 +1,75 @@
 var SegmentVis   = require('segment-vis');
 var makeEditable = require('make-editable');
+var extend       = require('utils').extend;
 
 'use strict';
 
 var SegmentEdit = (function(super$0){"use strict";var PRS$0 = (function(o,t){o["__proto__"]={"a":t};return o["a"]===t})({},{});var DP$0 = Object.defineProperty;var GOPD$0 = Object.getOwnPropertyDescriptor;var MIXIN$0 = function(t,s){for(var p in s){if(s.hasOwnProperty(p)){DP$0(t,p,GOPD$0(s,p));}}return t};var SP$0 = Object.setPrototypeOf||function(o,p){if(PRS$0){o["__proto__"]=p;}else {DP$0(o,"__proto__",{"value":p,"configurable":true,"enumerable":false,"writable":true});}return o};var OC$0 = Object.create;if(!PRS$0)MIXIN$0(SegmentEdit, super$0);var proto$0={};
 
   function SegmentEdit() {
-    if (!(this instanceof SegmentEdit)) return new SegmentEdit;
+    if (!(this instanceof SegmentEdit)) { return new SegmentEdit; }
 
     super$0.call(this);
     // default editable properties
-    this.param('edits', ['x', 'y', 'width', 'height']);
+    this.params({
+      edits: ['x', 'y', 'width', 'height'],
+      handlerWidth: 3,
+      handlerOpacity: 0
+    });
   }if(super$0!==null)SP$0(SegmentEdit,super$0);SegmentEdit.prototype = OC$0(super$0!==null?super$0.prototype:null,{"constructor":{"value":SegmentEdit,"configurable":true,"writable":true}});DP$0(SegmentEdit,"prototype",{"configurable":false,"enumerable":false,"writable":false});
+
+  // add handler on segment shape
+  proto$0.update = function(data) {
+    var g = super$0.prototype.update.call(this, data);
+
+    g.append('line')
+      .attr('class', 'handle left')
+      .attr('stroke-width', this.param('handlerWidth'))
+      .attr('stroke-opacity', this.param('handlerOpacity'));
+
+    g.append('line')
+      .attr('class', 'handle right')
+      .attr('stroke-width', this.param('handlerWidth'))
+      .attr('stroke-opacity', this.param('handlerOpacity'));
+  };
+
+  proto$0.draw = function(el) {
+    el = super$0.prototype.draw.call(this, el);
+    var accessors = this.getAccesors();
+
+    el.selectAll('.handle.left')
+      .attr('x1', accessors.lhx)
+      .attr('x2', accessors.lhx)
+      .attr('y1', accessors.y)
+      .attr('y2', accessors.hh)
+      .style('stroke', accessors.color);
+
+    el.selectAll('.handle.right')
+      .attr('x1', accessors.rhx)
+      .attr('x2', accessors.rhx)
+      .attr('y1', accessors.y)
+      .attr('y2', accessors.hh)
+      .style('stroke', accessors.color);
+  };
+
+  proto$0.getAccesors = function() {var this$0 = this;
+    var accessors = super$0.prototype.getAccessors.call(this);
+
+    var _handlerWidth = parseInt(this.param('handlerWidth'), 10)
+    var _halfHandler = _handlerWidth * 0.5;
+
+    // handler positions
+    var hh  = function(d)  { return accessors.y(d) + accessors.h(d); }
+    var lhx = function(d)  { return accessors.x(d) + _halfHandler; }
+    var rhx = function(d)  {
+      var width = accessors.w(d);
+
+      return (width < (_handlerWidth * 2)) ?
+        accessors.x(d) + _handlerWidth + this$0.__minWidth : accessors.x(d) + width - _halfHandler;
+    }
+
+    return extend(accessors, { hh: hh, lhx: lhx, rhx: rhx });
+  };
 
   // logic performed to select an item from the brush
   proto$0.brushItem = function(extent, mode) {
@@ -63,7 +121,8 @@ var SegmentEdit = (function(super$0){"use strict";var PRS$0 = (function(o,t){o["
 
   // checks if the clicked item is one of our guys
   proto$0.clicked = function(item) {
-    return item.classList.contains('seg') || item.tagName === 'line';
+    return item.classList.contains(this.param('rect-class')) ||
+           item.tagName === 'line';
   };
 
   // mouse drag event switcher depending on drag (left|right|block) levels
@@ -83,7 +142,9 @@ var SegmentEdit = (function(super$0){"use strict";var PRS$0 = (function(o,t){o["
     var d = res.d;
     var delta = res.event;
     var item = res.target;
+
     var xScale = this.base.xScale;
+    var yScale = this.yScale;
 
     var constrains = this.param('edits');
     var canX = !!~constrains.indexOf('x');
@@ -103,34 +164,44 @@ var SegmentEdit = (function(super$0){"use strict";var PRS$0 = (function(o,t){o["
       this.base.svg.classed('handle-drag', true);
     }
 
-    var duration = xScale(_duration(d));
-    var begin = xScale(_start(d));
+    var width = xScale(_duration(d));
+    var posX = xScale(_start(d));
 
     // handle resize
-    if (mode === 'l' && canW) duration -= delta.dx; // px
-    if (mode === 'r' && canW) duration += delta.dx; // px
+    if (mode === 'l' && canW) width -= delta.dx; // px
+    if (mode === 'r' && canW) width += delta.dx; // px
 
     // apply duration when editing through the handles
     if ((mode === 'l' || mode === 'r') && canW) {
-      if (duration < 1) { duration = 1; }
-      _duration(d, xScale.invert(duration));
+      if (width < 1) { width = 1; }
+      _duration(d, xScale.invert(width));
     }
 
     if (mode === 'l' && canW) {
-      begin += delta.dx;
-      _start(d, xScale.invert(begin));
+      posX += delta.dx;
+      _start(d, xScale.invert(posX));
     }
 
     // handle move
     if (mode === 'mv' && canX) {
-      begin += delta.dx;
-      _start(d, xScale.invert(begin));
+      var minX = 0;
+      var maxX = this.base.width();
+      var targetX = posX + delta.dx;
+
+      if (targetX >= minX && (targetX + width) <= maxX) {
+        posX = targetX;
+        _start(d, xScale.invert(posX));
+      }
     }
 
     if (mode === 'mv' && canY) {
-      var y = this.yScale(_y(d));
-      y += delta.dy;
-      _y(d, this.yScale.invert(y));
+      var posY = this.yScale(_y(d)); // - (this.param('height') - this.yScale(this.height()(d)));
+      var minY = 0;
+      var maxY = this.param('height');
+      // console.log(posY)
+
+      posY += delta.dy;
+      _y(d, this.yScale.invert(posY));
     }
 
     // redraw visualization
