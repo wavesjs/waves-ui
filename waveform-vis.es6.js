25cf52f1c14948c8c36878c196a44637b29f7f1d
b-ma <benjamin.matuszewski@gmail.com>
2014-11-26 19:17:55 +0100

    refacto

--- waveform-vis.es6.js
+++ waveform-vis.es6.js
@@ -3,11 +3,13 @@ var uniqueId = require('utils').uniqueId;
 var LayerVis = require('layer-vis');
 var pck      = require('./package.json');
 var fs       = require('fs');
+var renderingStrategies = require('./lib/rendering-strategies');
+var minMax   = require('./lib/min-max');
 
 'use strict';
 
 var workerBlob = new Blob(
-  [fs.readFileSync(__dirname + '/resampler-worker.js', 'utf-8')],
+  [fs.readFileSync(__dirname + '/lib/resampler-worker.js', 'utf-8')],
   { type: 'text/javascript' }
 );
 
@@ -23,59 +25,110 @@ class WaveformVis extends LayerVis {
       type: name,
       id: uniqueId(name),
       downSampleStrategy: 'minMax',
-      yDomain: [-1, 1] // default yDomain for audioBuffer
+      renderingStrategy: 'svg',
+      yDomain: [-1, 1], // default yDomain for audioBuffer
+      triggerUpdateZoomDelta: 0.01,
+      useWorker: true
     };
-
-    // `data` assume a binary array as input
-    this.__resampledViews = {};
 
     this.params(defaults);
     this.color('#000000');
     this.sampleRate(44100);
+    // init zoom factor to 1
+    this.currentZoomFactor = 1;
+  }
 
-    // init worker resampler
-    this.resampler = new Worker(window.URL.createObjectURL(workerBlob));
-    this.resampler.addEventListener('message', this.resamplerResponse.bind(this), false);
+  // get number of sample per timeline pixels - aka. windowSize
+  getSamplesPerPixel() {
+    var timelineDomain = this.base.xScale.domain();
+    var timelineDuration = timelineDomain[1] - timelineDomain[0];
+    var timelineWidth = this.base.width();
+    var sampleRate = this.sampleRate();
+
+    return (timelineDuration * sampleRate()) / timelineWidth;
   }
 
   onload() {
-    this.downSample();
+    // bind rendering strategy
+    var strategy = renderingStrategies[this.param('renderingStrategy')];
+    this._update = strategy.update.bind(this);
+    this._draw   = strategy.draw.bind(this);
+
+    // create partial xxScale
+    this.xxScale = this.d3.scale.linear()
+      .range([0, this.duration()()]);
+
+    // init worker
+    if (this.param('useWorker')) { this.initWorker(); }
   }
 
-  // call the resampler worker
-  downSample() {
-    var width = this.base.width();
-    var duration = this.duration();
-    var sampleRate = this.sampleRate();
-    var samplesPerPixel = Math.floor((duration() * sampleRate()) / width);
-    // @TODO later
-    // var halfSamplesPerPixel =  samplesPerPixel / 2; // double precision for zooming
-    samplesPerPixel = samplesPerPixel / 4;
+  initWorker() {
+    this.resampler = new Worker(window.URL.createObjectURL(workerBlob));
+    var onResponse = this.resamplerResponse.bind(this);
+    this.resampler.addEventListener('message', onResponse, false);
 
     var message = {
-      cmd: this.param('downSampleStrategy'),
-      samplesPerPixel: samplesPerPixel,
-      buffer: this.data()
+      cmd: 'initialize',
+      buffer: this.data(),
+      minMax: minMax.toString()
     };
 
-    // if cache exists don't call resampler again
-    // if (this.__downSampledViews[smaplesPerPixel])
     this.resampler.postMessage(message, [message.buffer]);
-    // `data()` is empty now -> set it in resampler callback
-    // console.log(new Float32Array(this.data()));
+  }
+
+  // call the resampler worker or online minMax 
+  // according to `this.param('useWorker')`
+  downSample() {
+    var range = this.base.xScale.range();
+    var width = range[1] - range[0];
+    var extractAtTimes = [];
+
+    // define all times where a minMax snapshot must be done
+    for (let pixel = 0; pixel < width; pixel++) {
+      var timelineTimeStart = this.base.xScale.invert(pixel);
+      extractAtTimes.push(timelineTimeStart);
+    }
+
+    // define center of the y domain for default values
+    var yDomain = this.yScale.domain(); // not this
+    var defaultValue = (yDomain[0] + yDomain[1]) / 2;
+    var sampleRate = this.sampleRate()();
+    var windowSize = this.getSamplesPerPixel();
+
+    if (this.param('useWorker')) {
+      var message = {
+        cmd: 'downSample',
+        extractAtTimes: extractAtTimes,
+        sampleRate: sampleRate,
+        windowSize: windowSize,
+        defaultValue: defaultValue
+      };
+
+      this.resampler.postMessage(message);
+    } else {
+      var data = this.data();
+      var buffer = data instanceof ArrayBuffer ? new Float32Array(data) : data;
+
+      var downSampledView = minMax(
+        buffer, 
+        extractAtTimes, 
+        sampleRate, 
+        windowSize, 
+        defaultValue
+      );
+
+      this.setDownSample(downSampledView);
+    }
   }
 
   // is called by the resampler worker when done
   // @NOTE is this method really needed
   resamplerResponse(message) {
     var data = message.data;
-    // set back buffer into data
-    this.data(data.buffer);
 
     switch (data.cmd) {
-      case 'median':
-      case 'minMax':
-        this.setDownSample(data);
+      case 'downSample':
+        this.setDownSample(data.downSampledView);
         break;
       default:
         throw new Error('Resampler unkown command: ' + data.msg);
@@ -85,84 +138,45 @@ class WaveformVis extends LayerVis {
 
   // cache the down sampling result and create some scale
   setDownSample(data) {
-    var downSampledView = data.downSampledView;
-    // cache down sampled data view
-    this.__resampledViews[data.samplesPerPixel] = downSampledView;
-    this.currentSamplesPerPixel = data.samplesPerPixel;
-    // create scale from resampled data domain to base domain
-    this.xxScale = this.d3.scale.linear()
-      .domain([0, downSampledView.length])
+    // update xxScale according to new base.xScale.domain and data.length
+    this.xxScale
+      .domain([0, data.length])
       .range(this.base.xScale.domain());
 
-    this.draw();
+    // update cache
+    this.cache(data);
+    this.draw(data);
   }
 
   // zoom - needs to be tested again
-  xZoom(zoom) {
-    var val = zoom.value;
-    this.factor = zoom.factor;
-    var cache = this.__resampledViews[this.currentSamplesPerPixel];
-    var min = this.base.xScale.domain()[0],
-        max = this.base.xScale.domain()[1];
-    // this.draw(cache.slice(this.xxScale.invert(min), this.xxScale.invert(max)));
-    this.draw();
+  xZoom(e) {
+    // @TODO caching system must be improved:
+    // - different trigger updates according to zoom in or out
+    // - force update when only sliding without zooming
+    var triggerUpdateZoomDelta = this.param('triggerUpdateZoomDelta');
+    var deltaZoom = Math.abs(this.currentZoomFactor - e.factor);
+    // if not enought zoom delta render cached data
+    if (deltaZoom < triggerUpdateZoomDelta) {
+      return this.draw(this.cache()());
+    }
+
+    this.currentZoomFactor = e.factor;
+    this.downSample();
   }
 
   // display methods
   update() {
-    this.g.append('foreignObject')
-      .attr('width', this.param('width'))
-      .attr('height', this.param('height'))
-      .append('xhtml:canvas')
-        .attr('width', this.param('width'))
-        .attr('height', this.param('height'))
-        .classed(this.param('unitClass'), true);
+    this._update();
   }
 
   draw(data) {
-    data = data || this.__resampledViews[this.currentSamplesPerPixel];
-
-    if (!data) { return; }
-
-    var el = this.g.selectAll('.' + this.param('unitClass')).node();
-    var ctx = this.ctx = this.ctx || el.getContext('2d');
-
-    var width = ctx.canvas.width = this.param('width');
-    var height = ctx.canvas.height = this.param('height');
-    var center = height / 2;
-    var color = this.color();
-
-    var length = data.length - 2;
-
-    var _y = (d) => { return this.yScale(d) };
-    var _x = (i) => { return this.base.xScale(this.xxScale(i)); }
-
-    ctx.beginPath();
-    ctx.strokeStyle = color();
-    ctx.fillStyle   = color();
-    ctx.moveTo(0, center);
-
-    for (var i = 0; i < length; i += 2) {
-      var x = i / 2;
-
-      var x1 = _x(i);
-      var x2 = _x(i + 2);
-
-      ctx.moveTo(x1, _y(data[i]));
-      ctx.lineTo(x1, _y(data[i + 1]));
-      ctx.lineTo(x2, _y(data[i + 3]));
-      ctx.lineTo(x2, _y(data[i + 2]));
-      ctx.lineTo(x1, _y(data[i]));
-    }
-
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
+    if (!data) { return this.downSample(); }
+    this._draw(data);
   }
 
 }
 
 // data accessors
-getSet(WaveformVis.prototype, ['color', 'sampleRate', 'duration']);
+getSet(WaveformVis.prototype, ['color', 'sampleRate', 'duration', 'cache']);
 
 module.exports = WaveformVis;
