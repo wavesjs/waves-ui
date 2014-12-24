813012f48e0b4c68a4d459743654b49d24f9330f
b-ma <benjamin.matuszewski@gmail.com>
2014-12-24 13:17:15 +0100

    remove `extend` and `getSet`

--- utils.es6.js
+++ utils.es6.js
@@ -6,38 +6,54 @@ utils.isFunction = function(func) {
   return Object.prototype.toString.call(func) === '[object Function]';
 };
 
+// @TODO - remove in favor of `Object.assign`
+// @NOTE done in ui components
+// utils.extend = function extend() {
+//   // this can probably improved in es6
+//   var args = Array.prototype.slice.call(arguments);
+//   var host = args.shift();
+//   var copy = args.shift();
+
+//   for (var i in copy) { host[i] = copy[i]; }
+//   args.unshift(host);
+
+//   if (args.length > 1) { return extend.apply(null, args); }
+//   return host;
+// };
+
 var explode = function(items, cb) {
-  if (Array.isArray(items)) {
-    items.forEach(cb);
-  } else {
-    cb(items);
+  if (!Array.isArray(items)) { 
+    items = [items] 
   }
+  
+  items.forEach(cb);
 };
 
+// @TODO - remove when all occurence removed in dependencies
+// @NOTE done in ui components
 // combined accessors
-utils.getSet = function getSet(obj, props = null, valueMode = false){
-  if (!props) throw new Error('Property name is mandatory.');
+// utils.getSet = function getSet(obj, props = null, valueMode = false){
+//   if (!props) throw new Error('Property name is mandatory.');
 
-  var add = (p = null) => {
-    var _prop = '_' + p;
-    if (!obj.hasOwnProperty(_prop)) obj[_prop] = null;
+//   var add = (p = null) => {
+//     var _prop = '_' + p;
+//     if (!obj.hasOwnProperty(_prop)) obj[_prop] = null;
 
-    obj[p] = function(value = null) {
-      if (value === null) return this[_prop];
+//     obj[p] = function(value = null) {
+//       if (value === null) return this[_prop];
 
-      if (!utils.isFunction(value) && !valueMode) {
-        this[_prop] = function() { return value; };
-      } else {
-        this[_prop] = value;
-      }
+//       if (!utils.isFunction(value) && !valueMode) {
+//         this[_prop] = function() { return value; };
+//       } else {
+//         this[_prop] = value;
+//       }
 
-      return this;
-    };
-  };
+//       return this;
+//     };
+//   };
 
-  explode(props, (p) => add(p));
-
-};
+//   explode(props, (p) => add(p));
+// };
 
 // combined accessors
 utils.accessors = {
@@ -123,17 +139,31 @@ utils.uniqueId = function(prefix = '') {
   return id;
 };
 
-utils.extend = function extend() {
-  // this can probably improved in es6
-  var args = Array.prototype.slice.call(arguments);
-  var host = args.shift();
-  var copy = args.shift();
+// style injection
+var _sheet;
 
-  for (var i in copy) { host[i] = copy[i]; }
-  args.unshift(host);
+function createStyleSheet() {
+  var el = document.createElement('style');
+  // webkit hack: cf. http://davidwalsh.name/add-rules-stylesheets
+  el.appendChild(document.createTextNode(''));
+  document.body.appendChild(el);
+  _sheet = el.sheet;
+}
 
-  if (args.length > 1) { return extend.apply(null, args); }
-  return host;
+utils.addCssRule = function(selector, rules, position = 0) {
+  if (!_sheet) { createStyleSheet(); }
+
+  var rule = Object.keys(rules).map((key) => {
+    return key + ':' + rules[key];
+  }).join(';');
+
+  rule = selector + '{' + rule + '}';
+  _sheet.insertRule(rule, position);
+}
+
+// 
+utils.toFront = function(item) {
+  item.parentNode.appendChild(item);
 };
 
 utils.UILoop = require('./lib/ui-loop');
--- utils.js
+++ utils.js
@@ -6,38 +6,54 @@ utils.isFunction = function(func) {
   return Object.prototype.toString.call(func) === '[object Function]';
 };
 
+// @TODO - remove in favor of `Object.assign`
+// @NOTE done in ui components
+// utils.extend = function extend() {
+//   // this can probably improved in es6
+//   var args = Array.prototype.slice.call(arguments);
+//   var host = args.shift();
+//   var copy = args.shift();
+
+//   for (var i in copy) { host[i] = copy[i]; }
+//   args.unshift(host);
+
+//   if (args.length > 1) { return extend.apply(null, args); }
+//   return host;
+// };
+
 var explode = function(items, cb) {
-  if (Array.isArray(items)) {
-    items.forEach(cb);
-  } else {
-    cb(items);
+  if (!Array.isArray(items)) { 
+    items = [items] 
   }
+  
+  items.forEach(cb);
 };
 
+// @TODO - remove when all occurence removed in dependencies
+// @NOTE done in ui components
 // combined accessors
-utils.getSet = function getSet(obj){var props = arguments[1];if(props === void 0)props = null;var valueMode = arguments[2];if(valueMode === void 0)valueMode = false;
-  if (!props) throw new Error('Property name is mandatory.');
+// utils.getSet = function getSet(obj, props = null, valueMode = false){
+//   if (!props) throw new Error('Property name is mandatory.');
 
-  var add = function()  {var p = arguments[0];if(p === void 0)p = null;
-    var _prop = '_' + p;
-    if (!obj.hasOwnProperty(_prop)) obj[_prop] = null;
+//   var add = (p = null) => {
+//     var _prop = '_' + p;
+//     if (!obj.hasOwnProperty(_prop)) obj[_prop] = null;
 
-    obj[p] = function() {var value = arguments[0];if(value === void 0)value = null;
-      if (value === null) return this[_prop];
+//     obj[p] = function(value = null) {
+//       if (value === null) return this[_prop];
 
-      if (!utils.isFunction(value) && !valueMode) {
-        this[_prop] = function() { return value; };
-      } else {
-        this[_prop] = value;
-      }
+//       if (!utils.isFunction(value) && !valueMode) {
+//         this[_prop] = function() { return value; };
+//       } else {
+//         this[_prop] = value;
+//       }
 
-      return this;
-    };
-  };
+//       return this;
+//     };
+//   };
 
-  explode(props, function(p)  {return add(p)});
-
-};
+//   explode(props, (p) => add(p));
+// };
 
 // combined accessors
 utils.accessors = {
@@ -123,17 +139,31 @@ utils.uniqueId = function() {var prefix = arguments[0];if(prefix === void 0)pref
   return id;
 };
 
-utils.extend = function extend() {
-  // this can probably improved in es6
-  var args = Array.prototype.slice.call(arguments);
-  var host = args.shift();
-  var copy = args.shift();
+// style injection
+var _sheet;
 
-  for (var i in copy) { host[i] = copy[i]; }
-  args.unshift(host);
+function createStyleSheet() {
+  var el = document.createElement('style');
+  // webkit hack: cf. http://davidwalsh.name/add-rules-stylesheets
+  el.appendChild(document.createTextNode(''));
+  document.body.appendChild(el);
+  _sheet = el.sheet;
+}
 
-  if (args.length > 1) { return extend.apply(null, args); }
-  return host;
+utils.addCssRule = function(selector, rules) {var position = arguments[2];if(position === void 0)position = 0;
+  if (!_sheet) { createStyleSheet(); }
+
+  var rule = Object.keys(rules).map(function(key)  {
+    return key + ':' + rules[key];
+  }).join(';');
+
+  rule = selector + '{' + rule + '}';
+  _sheet.insertRule(rule, position);
+}
+
+// 
+utils.toFront = function(item) {
+  item.parentNode.appendChild(item);
 };
 
 utils.UILoop = require('./lib/ui-loop');
