98fc7ebe1ec246daa85abc6de94ba33e95900ee9
b-ma <benjamin.matuszewski@gmail.com>
2015-06-29 16:14:56 +0200

    fixed stretchRatio

--- es6/core/time-context.js
+++ es6/core/time-context.js
@@ -1,36 +1,32 @@
-// @FIXME there is a problem with the stretch:
-// how does it must be applyed ?
-// should we maintain some `absoluteStart`, `absoluteDuration`, etc... values ?
-
-// @NOTE: separate timeline's with from the scale,
-// => only define #pixels for one seconds (kind of real scale) ?
-
+/**
+ *  @class TimeContext
+ *
+ *  represent a time segment and its scale to the pixel domain
+ *  the timeContexts are organised in a tree structure,
+ *  with the timeline's TimeContext on top
+ *
+ *  @WARNING: the tree works with two level, but probably wont with more depth
+ */
 class TimeContext {
   constructor(parent = null) {
     this.parent = parent;
     this._children = [];
 
-    this._xScale = null; // inherits from parent context
+    this._xScale = null;
     this._originalXScale = null;
 
     this.start = 0;
     this.duration = (parent !== null) ? parent.duration : 1;
     this.offset = 0;
     this._stretchRatio = 1;
-    // @NOTE: need an `absoluteStretchRatio` ?
 
     if (this.parent) {
       this.parent._children.push(this);
     }
   }
 
-  // attempt to get a solution to the stretch problem
-  // get normalizedStart() {
-  //   return this.start / this.stretchRatio;
-  // }
-
   /**
-   * @return {Function} the closest available xScale in the tree
+   * @return {Function} the closest available xScale in the TimeContext tree
    */
   get xScale() {
     if (this.parent && !this._xScale) {
@@ -44,17 +40,14 @@ class TimeContext {
     this._xScale = xScale;
   }
 
-  // read only
-  // @NOTE not used anymore outside this object, was used to have a proper start
-  // from an inner context with it's own `stretchRatio` but creates many bad side effects
-  // => find another strategy => use _context.parent.xScale
+  // returns the xScale as defined in the timeline without stretching
   get originalXScale() {
     // lazy bind originalXScale on top of the tree
     if (!this.parent && !this._originalXScale) {
       this._originalXScale = this._xScale;
     }
 
-    // returns the closest available xScale in the tree
+    // returns the closest available xScale in the tree (aka the timeline)
     if (this.parent) {
       return this.parent.originalXScale;
     } else {
@@ -66,29 +59,29 @@ class TimeContext {
     return this._stretchRatio;
   }
 
-  // @FIXME: inconsistencies occur
-  // when stretching parent and when child.stretchRatio != 1
   set stretchRatio(ratio) {
     // do not remove xScale on top of the graph
-    if (
-      ratio === 1 &&
-      this.parent &&
-      this.parent.stretchRatio === 1
-    ) {
+    if (ratio === 1 && this.parent) {
       this._xScale = null;
     } else {
       const xScale = this.originalXScale.copy();
       const [min, max] = xScale.domain();
-      const diff = (max - min) / ratio;
-      xScale.domain([min, min + diff]);
 
+      let diff = (max - min) / ratio;
+
+      if (this.parent) {
+        diff = diff / this.parent.stretchRatio;
+      }
+
+      xScale.domain([min, min + diff]);
       this._xScale = xScale;
     }
 
-    const ratioChange = ratio / this._stretchRatio;
     this._stretchRatio = ratio;
 
     // propagate change to children who have their own stretchRatio
+    const ratioChange = ratio / (this._stretchRatio);
+
     this._children.forEach(function(child) {
       if (child._xScale) {
         child.stretchRatio = child.stretchRatio * ratioChange;
