a71f4863ec0a1765f086e36b02ec4e5fe40e7f5e
ouhouhsami <samuel.goldszmidt@gmail.com>
2015-06-29 15:01:04 +0200

    time context test

--- tests/core/time-context.js
+++ tests/core/time-context.js
@@ -1,10 +1,25 @@
 const assert = require('assert');
 const TimeContext = require('../../es6/core/time-context');
+const d3Scale = require('d3-scale');
 
 describe('TimeContext', function(){
     describe('instanciation, getters and setters', function(){
-        let tc = new TimeContext();
-        // not HERE offsetX don't change when stretchRatio change ...
-        // change scale on layer (and timeline) and check that offset don't move
+        it('should get and set xScale correctly', function(){
+            let parentTimeContext = new TimeContext();
+            let childTimeContext1 = new TimeContext(parentTimeContext);
+            let childTimeContext2 = new TimeContext(parentTimeContext);
+            childTimeContext2.xScale = 'foo-function';
+            assert.equal(parentTimeContext.xScale, null);
+            assert.equal(childTimeContext1.xScale, null);
+            assert.equal(childTimeContext2.xScale, 'foo-function');
+            parentTimeContext.xScale = 'bar-function'
+            assert.equal(parentTimeContext.xScale, 'bar-function');
+            assert.equal(childTimeContext1.xScale, 'bar-function');
+            assert.equal(childTimeContext2.xScale, 'foo-function');
+        })
+        it('should get correct stretchratio', function(){
+            let parentTimeContext = new TimeContext();
+            assert.equal(parentTimeContext.stretchRatio, 1);
+        })
     })
 })
