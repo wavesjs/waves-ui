5b4a586dc9128e16522cd721d44c1da9cbe0b44e
ouhouhsami <samuel.goldszmidt@gmail.com>
2015-06-30 17:40:10 +0200

    improve timeline test

--- tests/core/timeline.js
+++ tests/core/timeline.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const Timeline = require('../../es6/core/timeline');
 
 describe('Timeline', function(){
-    describe('Container registration', function(){
+    describe('Container registration and manipulation', function(){
         it('should create a container with the rights default width and height', function(){
             let timelineDiv = document.createElement("div");
             document.body.appendChild(timelineDiv);
@@ -23,6 +23,13 @@ describe('Timeline', function(){
             assert.equal(boundingClientRect.width, width);
             assert.equal(timeline.params.duration, duration);
         });
+        it('should getContainerPerElement from element', function(){
+            let timelineDiv = document.createElement("div");
+            document.body.appendChild(timelineDiv);
+            let timeline = new Timeline();
+            timeline.registerContainer('foo', timelineDiv);
+            assert.equal(timeline.getContainerPerElement(timelineDiv).id, 'foo')
+        })
     });
     describe('Global Timeline rendering options', function(){
         it('should set width of the timeline correctly', function(){
@@ -34,6 +41,26 @@ describe('Timeline', function(){
             timeline.registerContainer('foo', timelineDiv);
             const boundingClientRect = timeline.containers.foo.svgElement.getBoundingClientRect();
             assert.equal(boundingClientRect.width, 800);
+        })
+    })
+    describe('Manipulate layers inside a timeline', function(){
+        it('should get the layer from getGroup', function(){
+            let timelineDiv = document.createElement("div");
+            document.body.appendChild(timelineDiv);
+            let timeline = new Timeline();
+            timeline.registerContainer('foo', timelineDiv);
+            let layer1 = {'name': 'layer1'};
+            timeline.addLayer(layer1, 'foo', 'bar');
+            assert.deepEqual(timeline.getGroup('bar'), [layer1]);
+        })
+        it('should getContainer from layer', function(){
+            let timelineDiv = document.createElement("div");
+            document.body.appendChild(timelineDiv);
+            let timeline = new Timeline();
+            timeline.registerContainer('foo', timelineDiv);
+            let layer1 = {'name': 'layer1'};
+            timeline.addLayer(layer1, 'foo');
+            assert.equal(timeline.getLayerContainer(layer1).id, 'foo')
         })
     })
 });
