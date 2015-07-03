c553708fe027412b33c15429c2106e33cc896638
b-ma <benjamin.matuszewski@gmail.com>
2015-07-03 18:09:24 +0200

    small fix

--- es6/timeline-states/centered-zoom-state.js
+++ es6/timeline-states/centered-zoom-state.js
@@ -46,7 +46,7 @@ class CenteredZoomState extends BaseState {
     // example keep in container when zoomed out
     if (timeContext.stretchRatio < 1) {
       const minOffset = timeContext.xScale.invert(0);
-      const maxOffset = timeContext.xScale.invert(timeline.containersWidth - timeContext.xScale(timeContext.duration));
+      const maxOffset = timeContext.xScale.invert(timeline.containersWidth - timeContext.xScale(timeContext.containersDuration));
 
       timeContext.offset = Math.max(timeContext.offset, minOffset);
       timeContext.offset = Math.min(timeContext.offset, maxOffset);
