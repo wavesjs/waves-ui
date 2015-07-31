18ac1db48b795d7f58dcbeb6e6328e960dbcbb4e
b-ma <benjamin.matuszewski@gmail.com>
2015-07-31 16:35:52 +0200

    simplified cursor

--- es6/shapes/cursor.js
+++ es6/shapes/cursor.js
@@ -19,25 +19,19 @@ export default class Cursor extends BaseShape {
   render(renderingContext) {
     if (this.$el) { return this.$el; }
 
-    console.log(this.params);
-
     this.$el = document.createElementNS(ns, 'line');
     this.$el.setAttributeNS(null, 'x', 0);
     this.$el.setAttributeNS(null, 'y1', 0);
     this.$el.setAttributeNS(null, 'y2', renderingContext.height);
     this.$el.setAttributeNS(null, 'shape-rendering', 'crispEdges');
-    this.$el.setAttributeNS(null, 'stroke', this.params.color);
+    this.$el.style.stroke = this.params.color;
 
     return this.$el;
   }
 
   update(renderingContext, datum, index) {
     const x = renderingContext.timeToPixel(this.x(datum));
-    const color = this.params.color;
-    // console.log(datum, x);
-
     this.$el.setAttributeNS(null, 'transform', `translate(${x}, 0)`);
-    this.$el.style.stroke = color;
   }
 
   // not selectable with a drag
