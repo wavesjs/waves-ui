3420c8bb9f5d1ca2131c00a8cbb09ad4d131af84
b-ma <benjamin.matuszewski@gmail.com>
2015-11-03 12:56:43 +0100

    updated documentation

--- dist/core/namespace.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports['default'] = 'http://www.w3.org/2000/svg';
-module.exports = exports['default'];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy91dGlscy9zY2FsZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7cUJBQWUsNEJBQTRCIiwiZmlsZSI6InNyYy91dGlscy9zY2FsZXMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAnaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnO1xuIl19
