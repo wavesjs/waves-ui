ff7326625c3e9583180369a1542f5a3e7e75bdb1
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-07-08 08:16:47 +0200

    ignored dist folder

--- dist/core/namespace.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports['default'] = 'http://www.w3.org/2000/svg';
-module.exports = exports['default'];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9jb3JlL25hbWVzcGFjZS5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztxQkFBZSw0QkFBNEIiLCJmaWxlIjoic3JjL2NvcmUvbmFtZXNwYWNlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgJ2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJztcbiJdfQ==
