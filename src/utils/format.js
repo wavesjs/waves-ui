b272fbfb4b71b19d78ae78c5011435bb6568472c
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-08-07 15:28:16 +0200

    simplfied export

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,19 +1,16 @@
+
 /**
- * Formatting helpers functions.
+ * Add `sign` to the left of a given `input` until it matches match `length`
+ *
+ * @param {String} input - String to format
+ * @param {String} sign - Character to add to the left
+ * @param {Number} length - Length of the output string
  */
-export default {
-  /**
-   * Add a `sign` to the left of a given `input` to match `length`
-   *
-   * @param {String} input - The string to format.
-   * @param {String} sign - The character to add to the left.
-   * @param {Number} length - The length of the output string.
-   */
-  padLeft(input, sign, length) {
-    input += '';
-    while (input.length < length) {
-      input = sign + input;
-    }
-    return input;
-  }
-};
+export const padLeft = (input, sign, length) => {
+  input += ''; // make sure we deal with a string
+
+  while (input.length < length)
+    input = sign + input;
+
+  return input;
+}
