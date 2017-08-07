f09113d13d7cc6c03ab6cbbbee9041b2da04a75d
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-08-07 15:16:32 +0200

    cosmetics

--- src/behaviors/marker-behavior.js
+++ src/behaviors/marker-behavior.js
@@ -6,11 +6,13 @@ import BaseBehavior from './base-behavior';
  *
  * [example usage](./examples/layer-marker.html)
  */
-export default class MarkerBehavior extends BaseBehavior {
+class MarkerBehavior extends BaseBehavior {
   edit(renderingContext, shape, datum, dx, dy, target) {
     const x = renderingContext.timeToPixel(shape.x(datum));
-    let targetX = (x + dx) > 0 ? x + dx : 0;
+    const targetX = (x + dx) > 0 ? x + dx : 0;
 
     shape.x(datum, renderingContext.timeToPixel.invert(targetX));
   }
 }
+
+export default MarkerBehavior;
